80be860c0f743a97ba7582e3f88aa463b1e00919
David Coelho <davidsocoelho@outlook.com>
2023-02-26 16:47:47 -0300

    Icon added

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -8,7 +8,7 @@ const Clients = () => (
     <div className={`w-full flex-wrap ${styles.flexCenter}`}>
       {clients.map((client)=>(
         <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
-          <img src={client.logo} alt="Client" className='sm:w-[192px] w-[100px] object-contain hover:brightness-[2.5] duration-300 cursor-pointer'/>
+          <img src={client.logo} alt="Client" className='sm:w-[192px] w-[100px] object-contain grayscale hover:grayscale-0 duration-300 cursor-pointer'/>
         </div>
       ))}
     </div>
